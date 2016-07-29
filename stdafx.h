02cdb225052c513fc28323bbb60f15edfb8f5902
Ben Xu <bjfuxuben@gmail.com>
2016-07-29 16:41:40 +0800

    stdafx.h

---
+++ stdafx.h
@@ -0,0 +1,27 @@
+//ansi c
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <time.h>
+#include <ctype.h>
+
+//other c
+
+//ansi c++
+#include <iostream>
+#include <algorithm>
+#include <queue>
+#include <set>
+#include <map>
+#include <stack>
+#include <string>
+#include <vector>
+#include <deque>
+#include <list>
+#include <functional>
+#include <numeric>
+
+//other c++
